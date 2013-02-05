bb0f3b53519e83c1a27d8fe722c44e09b6cf6016
Phaedra Deepsky <phae.deepsky@gmail.com>
2013-02-05 15:48:50 -0500

    Remove checkIP and getHost from App Delegate

--- StatusIP/AppDelegate.m
+++ StatusIP/AppDelegate.m
@@ -54,49 +54,6 @@
 	//[testClass getIPAndHost];
 }
 
-- (NSString *)checkIP
-{
-	NSString *theIP = [NSString string];
-	
-	NSError *error = nil;
-	NSMutableArray *arrayOfMatchStrings = [NSMutableArray array];
-	
-	NSURL *theURL = [NSURL URLWithString:@"http://checkip.dyndns.com/"];
-	NSString *thePage = [NSString stringWithContentsOfURL:theURL
-												 encoding:NSUTF8StringEncoding
-													error:&error];
-	NSLog(@"%@", thePage);
-	
-	if (!error) {
-		// The NSRegularExpression class is currently only available in the Foundation framework of iOS 4
-		NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}"
-																			   options:NSRegularExpressionDotMatchesLineSeparators | NSRegularExpressionAnchorsMatchLines
-																				 error:&error];
-		
-		NSArray *matchArray = [regex matchesInString:thePage options:0 range:NSMakeRange(0, [thePage length])];
-		
-		for (NSTextCheckingResult *match in matchArray) {
-			NSString *substringForMatch = [thePage substringWithRange:match.range];
-			[arrayOfMatchStrings addObject:substringForMatch];
-			
-		}
-		
-		if ([[arrayOfMatchStrings objectAtIndex:0] isKindOfClass:[NSString class]]) {
-			theIP = [arrayOfMatchStrings objectAtIndex:0];
-			
-		}
-	}
-	else {
-		NSLog(@"Oops... g %ld, %@", [error code], [error localizedDescription]);
-	}
-	return theIP;
-}
-
-//- (NSString *)getHostName
-//{
-//	//NSHost *theHost = [NSHost hostWithAddress:[self checkIP]];
-//	//return [theHost name];
-//}
 
 - (IBAction)showPopover:(id)sender
 {
