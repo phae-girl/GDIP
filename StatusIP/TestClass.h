5604338af98b9dde5cb97942b1b656c1c15cc132
Phaedra Deepsky <phae.deepsky@gmail.com>
2013-02-05 02:06:25 -0500

    Experiment works. This may be the way to handle the URL connexion

--- StatusIP/TestClass.h
+++ StatusIP/TestClass.h
@@ -8,6 +8,6 @@
 
 #import <Foundation/Foundation.h>
 
-@interface TestClass : NSObject
+@interface TestClass : NSObject <NSURLConnectionDelegate>
 
 @end
--- StatusIP/TestClass.m
+++ StatusIP/TestClass.m
@@ -8,15 +8,51 @@
 
 #import "TestClass.h"
 
-@implementation TestClass
+@implementation TestClass {
+	NSURL *baseURL;
+	NSURLRequest *request;
+	NSURLConnection *conn;
+	NSMutableData *responseData;
+	NSString *content;
+}
 
 - (id)init
 {
     self = [super init];
     if (self) {
         NSLog(@"TestClass Init!");
+		baseURL = [NSURL URLWithString:@"http://checkip.dyndns.com/"];
+		request = [NSURLRequest requestWithURL:baseURL];
+		conn = [NSURLConnection connectionWithRequest:request delegate:self];
+		responseData = [NSMutableData data];
+		
     }
     return self;
 }
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+    [responseData setLength:0];
+	NSLog(@"Connection rec'd response");
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    [responseData appendData:data];
+	NSLog(@"Connection rec'd data");
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    [[NSAlert alertWithError:error] runModal];
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    // Once this method is invoked, "responseData" contains the complete result
+	content = [[NSString alloc]initWithData:responseData encoding:NSUTF8StringEncoding];
+	NSLog(@"Connection finished data %@", content);
+}
+
 
 @end
