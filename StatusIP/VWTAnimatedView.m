80749a1e4889ba92c5ddfb44736e1d616caad50c
Phaedra Deepsky <phae.deepsky@gmail.com>
2013-02-26 12:40:33 -0500

    Add default values to VWTAnimatedView

--- StatusIP/VWTAnimatedView.m
+++ StatusIP/VWTAnimatedView.m
@@ -8,13 +8,27 @@
 
 #import "VWTAnimatedView.h"
 
+@interface VWTAnimatedView ()
+@property NSMutableDictionary *textAttributes;
+@property NSPoint textInsertionPoint;
+@property NSTimer *scroller, *colorFader;
+@property float redValue, greenValue, blueValue, alphaValue;
+@property NSString *textToBeDispalyed;
+
+
+@end
+
 @implementation VWTAnimatedView
 
 - (id)initWithFrame:(NSRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
-        // Initialization code here.
+        _redValue = 0.0f;
+		_greenValue = 0.0f;
+		_blueValue = 0.0f;
+		_alphaValue = 0.0f;
+		_textAttributes = [NSMutableDictionary dictionaryWithDictionary:@{NSForegroundColorAttributeName: [NSColor colorWithCalibratedRed:_redValue green:_greenValue blue:_blueValue alpha:_alphaValue], NSFontAttributeName: [NSFont systemFontOfSize:13]}];
     }
     
     return self;
