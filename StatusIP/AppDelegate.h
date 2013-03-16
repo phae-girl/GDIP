ddbe6cbc8612862703e9772a765c0064ebf2c0d4
Phaedra Deepsky <phae.deepsky@gmail.com>
2013-03-15 21:48:29 -0400

    Add and Connect Labels

--- StatusIP/AppDelegate.h
+++ StatusIP/AppDelegate.h
@@ -9,10 +9,12 @@
 @class VWTAnimatedView;
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
-@property (weak) IBOutlet NSPopover *popover;
-@property (weak) IBOutlet VWTAnimatedView *externalIPAddressView, *externalHostNameView, *localIPAddressView, *localHostNameView;
 @property (unsafe_unretained) IBOutlet NSWindow *someWindow;
+@property (weak) IBOutlet NSPopover *popover;
+@property (weak) IBOutlet NSTextField *externalIPAddressLabel, *externalHostNameLabel, *localIPAddressLabel, *localHostNameLabel;
+@property (weak) IBOutlet VWTAnimatedView *externalIPAddressView, *externalHostNameView, *localIPAddressView, *localHostNameView;
 @property NSWindowController *myWindow;
+
 
 @property NSMutableDictionary *addressesAndHostsForViews;
 
