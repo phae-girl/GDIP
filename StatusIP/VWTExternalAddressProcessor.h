c4b087a49790f185c1b4401827f1079234ee15b6
Phaedra Deepsky <phae@Phaedras-iMac.local>
2013-03-23 00:43:02 -0400

    Add Macros to External Address Processor for key names

--- StatusIP/VWTExternalAddressProcessor.h
+++ StatusIP/VWTExternalAddressProcessor.h
@@ -3,7 +3,11 @@
 //  Created by Phaedra Deepsky on 2013-02-05.
 
 #import <Foundation/Foundation.h>
-
+#define EAP_EXTERNAL_IP @"externalIPAddress"
+#define EAP_EXTERNAL_HOST @"externalHostName"
+#define EAP_LOCAL_IP @"localIPAddress"
+#define EAP_LOCAL_HOST @"localHostName"
+#define EAP_LOCALIZED_NAME @"localizedName"
 
 @protocol VWTExternalAddressProcessorDelegate <NSObject>
 @required
