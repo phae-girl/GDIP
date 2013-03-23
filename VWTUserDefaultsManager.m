7f316c22f588532227c60f27fcd28cc86b802248
Phaedra Deepsky <phae@Phaedras-iMac.local>
2013-03-23 00:17:52 -0400

    Refactor User Defaults Manager to use Array Extension

--- VWTUserDefaultsManager.m
+++ VWTUserDefaultsManager.m
@@ -16,28 +16,28 @@
 
 - (id)init
 {
-    self = [super init];
-    if (self) {
-        _checkBoxStateKeys = @[@"externalIPAddressCheckBoxState", @"externalHostNameCheckBoxState", @"localIPAddressCheckBoxState", @"localHostNameCheckBoxState"];
-        _userDefaults = [NSUserDefaults standardUserDefaults];
-        for (NSString *checkBoxState in self.checkBoxStateKeys) {
-            [self.userDefaults addObserver:self forKeyPath:checkBoxState options:NSKeyValueObservingOptionNew context:NULL];
-        }
-        
-        [self.userDefaults addObserver:self forKeyPath:@"masterCheckBoxState" options:NSKeyValueObservingOptionNew context:NULL];
+	self = [super init];
+	if (self) {
+		_checkBoxStateKeys = @[@"externalIPAddressCheckBoxState", @"externalHostNameCheckBoxState", @"localIPAddressCheckBoxState", @"localHostNameCheckBoxState"];
+		_userDefaults = [NSUserDefaults standardUserDefaults];
+		[self.checkBoxStateKeys each:^(NSString *checkBoxState){
+			[self.userDefaults addObserver:self forKeyPath:checkBoxState options:NSKeyValueObservingOptionNew context:NULL];
+		}];
+		
+		[self.userDefaults addObserver:self forKeyPath:@"masterCheckBoxState" options:NSKeyValueObservingOptionNew context:NULL];
 		[self verifyCopyButtonStatus];
-    }
-    return self;
+	}
+	return self;
 }
 
 - (void) observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
 {
 	if ([keyPath isEqualToString:@"masterCheckBoxState"] && [change[@"new"] boolValue]) {
-		for (NSString *checkBoxState in self.checkBoxStateKeys) {
+		[self.checkBoxStateKeys each:^(NSString *checkBoxState){
 			[self.userDefaults setBool:YES forKey:checkBoxState];
-		}
+		}];
 	}
-	
+	//Can't be used with block model because of break
 	for (NSString *checkBoxState in self.checkBoxStateKeys) {
 		if ([keyPath isEqualToString:checkBoxState] && ![change[@"new"] boolValue]) {
 			[self.userDefaults setBool:NO forKey:@"masterCheckBoxState"];
@@ -50,15 +50,16 @@
 
 - (void)verifyCopyButtonStatus
 {
-    for (NSString *checkBoxState in self.checkBoxStateKeys) {
-        if ([self.userDefaults boolForKey:checkBoxState]) {
-            [self.userDefaults setBool:YES forKey:@"copyButtonState"];
-            break;
-        }
-        else
-            [self.userDefaults setBool:NO forKey:@"copyButtonState"];
-        
-    }
+	//Can't be used with block model because of break
+	for (NSString *checkBoxState in self.checkBoxStateKeys) {
+		if ([self.userDefaults boolForKey:checkBoxState]) {
+			[self.userDefaults setBool:YES forKey:@"copyButtonState"];
+			break;
+		}
+		else
+			[self.userDefaults setBool:NO forKey:@"copyButtonState"];
+		
+	}
 }
 
 - (NSDictionary *)checkCopyableItems
